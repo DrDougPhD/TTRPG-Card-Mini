8ac1582fb4697b0efb8f36f622a5c6e97336b17a
Doug McGeehan <doug@kmnr.org>
2023-03-09 08:47:39 -0700

    bootstrap attempt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import './App.css'
 import PageHeader from './components/PageHeader'
@@ -7,12 +10,24 @@ import ImageCropper from './components/ImageCropper'
 import SettingsPanel from './components/SettingsPanel'
 
 const App = () => (
-  <>
-    <PageHeader></PageHeader>
-    <ImageQueue></ImageQueue>
-    <ImageCropper></ImageCropper>
-    <SettingsPanel></SettingsPanel>
-  </>
+  <Container fluid >
+    <Row>
+      <Col>
+        <PageHeader></PageHeader>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <ImageQueue></ImageQueue>
+      </Col>
+      <Col xs={8}>
+        <ImageCropper></ImageCropper>
+      </Col>
+      <Col>
+        <SettingsPanel></SettingsPanel>
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default App
--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function PageHeader () {
-  return <header></header>
+  return <header>Header</header>
 }
