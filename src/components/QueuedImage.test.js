5b31b82ecb655c4e752bf9cf8858ae2fb3a21d32
Doug McGeehan <doug@kmnr.org>
2023-03-10 10:00:05 -0700

    expecting image to be in document

--- src/components/QueuedImage.test.js
+++ src/components/QueuedImage.test.js
@@ -23,7 +23,7 @@ afterEach(() => {
   container = null
 });
 
-it('renders with or without a name', () => {
+it('creates an image element', () => {
   const imageDataUri = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII='
   const expectedFilename = '1x1.png'
   const image = new Image({
@@ -40,7 +40,9 @@ it('renders with or without a name', () => {
 
   const $img = $(container).find('img')
   
+  expect(container.querySelector('img')).toBeInTheDocument()
   expect($img.attr('alt')).toBe(expectedFilename)
+  expect($img.attr('src')).toBe(imageDataUri)
 
 
   // act(() => {
