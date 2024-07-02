1041e111da959b0df0e304b27e2520fd132afb78
Doug McGeehan <doug@kmnr.org>
2024-07-02 09:27:57 -0600

    adding command line arguments

--- src/cardstockminis/__init__.py
+++ src/cardstockminis/__init__.py
@@ -1,5 +1,54 @@
 __version__ = "1.0.0-alpha"
 
 
+import argparse
+import pathlib
+
+
+def main(args):
+    if not args.image.is_file():
+        raise FileNotFoundError(f"Could not find input image at {args.image}")
+    
+    output_file = args.output_file or args.image.with_suffix(".pdf")
+    output_file.parent.mkdir(exist_ok=True, parents=True)
+    
+
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(
+        description='Generate cardstock minis from input images.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        '--image', "-i",
+        type=pathlib.Path,
+        help='Input image to annotate as a cardstock mini'
+    )
+    parser.add_argument(
+        '--count', "-n",
+        type=int,
+        default=5,
+        help='Number of clones to generate'
+    )
+    parser.add_argument(
+        '--output-file', "-o",
+        type=pathlib.Path,
+        help='Output PDF to be printed. Default uses input image as the filename template.'
+    )
+    parser.add_argument(
+        '--text', "-t",
+        help='Text to write on top of image. Default uses capitalized input filename without its extension.'
+    )
+    parser.add_argument(
+        '--use-numbers',
+        action='store_true',
+        default=False,
+        help='Identify clones using numbers instead of letters'
+    )
+    parser.add_argument(
+        '--page-size',
+        choices=["letter", "a4"],
+        default="letter",
+        help='Size of page to be printed on'
+    )
+
+    main(args=parser.parse_args())
